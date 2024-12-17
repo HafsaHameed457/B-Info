dde48bfbc48fbeb23ed77adf9d3e9142c4b861c7
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-17 19:01:55 +0500

    recursive fibonacci

--- Math func/fibonnaci.js
+++ Math func/fibonnaci.js
@@ -18,3 +18,10 @@ function finbonacciNumber(n) {
   
   // Big O = O(N)
   
+  function fibonacciRecursive(n) {
+    if (n < 2) {
+      return n;
+    }
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  }
+  console.log(fibonacciRecursive(3));
