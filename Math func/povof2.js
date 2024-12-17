c97edf683a7e22d2b0e2557fe5877d6a6c433f25
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-17 17:26:30 +0500

    pow of 2

---
+++ Math func/povof2.js
@@ -0,0 +1,16 @@
+function povof2(n) {
+    if (n >= 2) {
+      while (n >= 2) {
+        if (n % 2 != 0) {
+          return false;
+        } else {
+          n /= 2;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+  
+  console.log(povof2(4));
+// Big 0 = O(n)  
