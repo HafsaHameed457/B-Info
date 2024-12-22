5b0f8d0267c9a6b29f06a3c29e1b678faa34b6a2
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-22 23:43:56 +0500

    algo addded

---
+++ algos/binarySearch.js
@@ -0,0 +1,23 @@
+const binarySearch = (arr, target) => {
+    let startIndex = 0,
+      endIndex = arr.length - 1;
+    let midIndex = 0;
+    if (arr.length === 0) return -1;
+    while (startIndex < endIndex) {
+      midIndex =
+        arr.length % 2 === 0
+          ? (startIndex + endIndex) / 2 - 1
+          : (startIndex + endIndex) / 2;
+      if (arr[midIndex] === target) {
+        return midIndex;
+      } else if (arr[midIndex] > target) {
+        endIndex = midIndex;
+      } else {
+        startIndex = midIndex;
+      }
+    }
+    return -1;
+  };
+  
+  console.log(binarySearch([1, 2, 3, 4, 5], 5));
+  
