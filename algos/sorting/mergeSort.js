83873ae76f94733aef37771b6b1c811e06aeb4a5
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-24 23:04:50 +0500

    another sorting algo

--- algos/sorting/mergeSort.js
+++ algos/sorting/mergeSort.js
@@ -3,5 +3,11 @@
 //Now repeatedly merge subarays to produce new sorted array
 
 const mergeSort=(arr)=>{
+    if(arr.length===0||arr.length===1){
+        return arr;
+    }
+    else{
+        let leftArr
+    }
     
 }
