d72d5b5ece522b29320d00f30211205f28a1f699
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-25 00:27:15 +0500

    selectionsort

--- algos/sorting/selectionSort.js
+++ algos/sorting/selectionSort.js
@@ -4,15 +4,22 @@
 // Then we find the smallest among remaining elements (or second smallest) and swap it with the second element.
 // We keep doing this until we get all elements moved to correct position.
 
-const selectionSort=(arr)=>{
-    if(arr.length===0||arr.length===1){
-        return arr
-    }
-    else{
-        for(let i=1;i<arr.length;i++){
-            let minIndex=0;
-
+const selectionSort = (arr) => {
+    if (arr.length === 0 || arr.length === 1) {
+      return arr;
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+          if (arr[j] < arr[minIndex]) {
+            let temp = arr[minIndex];
+            arr[minIndex] = arr[j];
+            arr[j] = temp;
+          }
         }
+      }
+      return arr;
     }
-
-}
+  };
+  
+  console.log(selectionSort([2, 3, 4, 5, 1, 7, 0]));
