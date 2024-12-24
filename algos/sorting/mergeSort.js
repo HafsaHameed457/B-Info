d1610fd8d7eace027aa41cbe3a854b294a02e6fa
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-24 22:56:55 +0500

    description in merge sort

---
+++ algos/sorting/mergeSort.js
@@ -0,0 +1,7 @@
+//Repeatedly divide an array into subarray until only one element remain in each array
+// it means each array is sorted
+//Now repeatedly merge subarays to produce new sorted array
+
+const mergeSort=(arr)=>{
+    
+}
