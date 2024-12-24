a93e6752dc878a57948cbb5331a82a96aae5b83a
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-24 23:51:57 +0500

    changes in merge sort

--- algos/sorting/mergeSort.js
+++ algos/sorting/mergeSort.js
@@ -2,35 +2,37 @@
 // it means each array is sorted
 //Now repeatedly merge subarays to produce new sorted array
 
-const mergeSort=(arr)=>{
-    if(arr.length===0||arr.length===1){
-        return arr;
+const mergeSort = (arr) => {
+    if (arr.length === 0 || arr.length === 1) {
+      return arr;
+    } else {
+      let leftArr = arr.slice(0, arr.length / 2);
+  
+      let rightArr = arr.slice(leftArr.length, arr.length);
+  
+      let sortedArr = [];
+      let sortedLeft = mergeSort(leftArr),
+        sortedRight = mergeSort(rightArr);
+  
+      while (sortedLeft.length != 0 && sortedRight.length != 0) {
+        if (sortedLeft[0] < sortedRight[0]) {
+          sortedArr.push(sortedLeft[0]);
+          sortedLeft.splice(0, 1);
+        } else {
+          sortedArr.push(sortedRight[0]);
+          sortedRight.splice(0, 1);
+        }
+      }
+      if (sortedLeft.length) {
+        sortedArr = [...sortedArr, ...sortedLeft];
+      }
+      if (sortedRight.length > 0) {
+        sortedArr = [...sortedArr, ...sortedRight];
+      }
+  
+      return sortedArr;
     }
-    else{
-        let leftArr=[...arr].splice(0,arr.length/2)
-            let rightArr=[...arr].splice(leftArr.length,0)
-            let sortedArr=[]
-            let sortedLeft=mergeSort(leftArr),sortedRight=mergeSort(rightArr)
-            while(sortedLeft.length!=0&&sortedRight.length!=0){
-                if(sortedLeft[0]<sortedRight[0]){
-                    sortedArr.push(sortedLeft[0])
-                    sortedLeft.splice(0,1)
-                }
-                else{
-                    sortedArr.push(sortedRight[0])
-                    sortedLeft.splice(0,1)
-                }
-            }
-            if(sortedLeft.length){
-                sortedArr=[...sortedArr,sortedRight]
-            }
-            if(sortedRight.length){
-                sortedArr=[...sortedArr,...sortedRight]
-            }
-
-return sortedArr;
-             
-        
-    }
-    
-}
+  };
+  
+  console.log(mergeSort([2, 3, 4, 5, 1, 7, 0]));
+  
