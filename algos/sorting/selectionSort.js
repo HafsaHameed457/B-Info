bf269fb89f0060ae810c183d4842c9e07f9b2ade
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-24 23:57:57 +0500

    koshish

--- algos/sorting/selectionSort.js
+++ algos/sorting/selectionSort.js
@@ -2,5 +2,11 @@ const selectionSort=(arr)=>{
     if(arr.length===0||arr.length===1){
         return arr
     }
+    else{
+        for(let i=1;i<arr.length;i++){
+            let minIndex=0;
+
+        }
+    }
 
 }
