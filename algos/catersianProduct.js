ee3fff5eee8976b675b54452dc5a3161a8e73dbd
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-25 23:00:25 +0500

    optimised solution

--- algos/catersianProduct.js
+++ algos/catersianProduct.js
@@ -5,10 +5,20 @@ const cartesianProduct=(arr1,arr2)=>{
     let product=[]
     for(let i=0;i<arr1.length;i++){
         for(let j=0;i<arr2.length;i++){
-            let newSet=[arr1[i],arr2[j]]
+            let newSet=[]
+            if(arr1[i]<arr2[j]){
+
+ newSet=[arr1[i],arr2[j]]
+            }else{
+ newSet=[arr2[j],arr1[i]]
+
+            }
+
+
             product.push(newSet)
         
         }   
     }
 return product;
 }
+console.log(cartesianProduct([1,2],[3,4]))
