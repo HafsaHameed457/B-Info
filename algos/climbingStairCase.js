7a6813b65e1899b6ed96c0fa314356b4dc40ad39
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-25 23:19:32 +0500

    climbing staircase

--- algos/climbingStairCase.js
+++ algos/climbingStairCase.js
@@ -1,1 +1,11 @@
 //For n steps in stairs give number of different ways to reach to the top you can take either 1 or 2 steps.
+
+const climbingStairCase = (steps) => {
+    let ways = [1, 2];
+    for (let i = 2; i < steps; i++) {
+      ways[i] = ways[i - 1] + ways[i - 2];
+    }
+    return ways[steps - 1];
+  };
+  
+  console.log(climbingStairCase(4));
