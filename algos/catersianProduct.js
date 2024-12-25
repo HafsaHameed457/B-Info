0b2919ad9b6e5fd23645898713fc31ca6a125d6f
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-25 22:53:12 +0500

    product brute force solution

--- algos/catersianProduct.js
+++ algos/catersianProduct.js
@@ -1,2 +1,14 @@
 //Given 2 finite sets fine cartesian product
 //Sets are represented as arrays in js
+
+const cartesianProduct=(arr1,arr2)=>{
+    let product=[]
+    for(let i=0;i<arr1.length;i++){
+        for(let j=0;i<arr2.length;i++){
+            let newSet=[arr1[i],arr2[j]]
+            product.push(newSet)
+        
+        }   
+    }
+return product;
+}
