e4bac2c2cb4e3c1663643b4b2f6faf125c3586c9
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-25 23:43:28 +0500

    algo

--- algos/towerOfHanoi.js
+++ algos/towerOfHanoi.js
@@ -1,6 +1,14 @@
+// if one disk move simply from first to last 
+// Shift n-1 disks from A to B using C (req)
+// Shift last disk from A to C
+// Shift n-1 disks from B to C using A (req)
+
 const towerOfHanoi=(n,rod1,rod3,rod2)=>{
-while(arr1.length||arr2.length){
-   if(arr1.length){
+while(rod1.length||rod2.length){
+   if(rod1.length&&!rod3.length){
+    rod3.push(rod1[0])
+   }
+   else{
 
    }
 }
